4a2baf07984836c6eff1636f26e990f86f74e8c4
CeliaMB1 <celiamurphybraunstein3@gmail.com>
2024-01-20 15:42:56 -0500

    first edits

---
+++ Data_Wrangle_Script.R
@@ -0,0 +1,15 @@
+##%######################################################%##
+#                ICP-MS Data Wrangle                       #
+#              Celia Murphy-Braunstein                     #
+#                  1/20/2024                               #                              
+#                                                          #
+##%######################################################%##
+
+# Libraries ----
+library(readxl)
+
+# Data import ----
+icp_ms <- read_xlsx("icp_ms_data.xlsx")
+head(icp_ms)
+
+
